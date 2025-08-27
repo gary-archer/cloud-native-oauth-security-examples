3ef2bbf8b37ba30e6ed8cfaadce8e4b6c80a0936
gary-archer <garyarcher36@gmail.com>
2025-08-27 08:37:09 +0100

    Android version updates

--- chapter-12-platform-specific-apps/android-app/app/build.gradle.kts
+++ chapter-12-platform-specific-apps/android-app/app/build.gradle.kts
@@ -5,24 +5,24 @@ plugins {
 }
 
 android {
-    compileSdk = 34
+    compileSdk = 36
     namespace = "com.example.demoapp"
 
     defaultConfig {
         applicationId = "com.example.demoapp"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+        jvmTarget = JavaVersion.VERSION_21.toString()
     }
 
     buildFeatures {
@@ -34,24 +34,24 @@ android {
 dependencies {
 
     // Kotlin extensions
-    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.core:core-ktx:1.17.0")
 
     // Jetpack compose
-    implementation("androidx.activity:activity-compose:1.9.2")
-    implementation(platform("androidx.compose:compose-bom:2024.09.01"))
+    implementation("androidx.activity:activity-compose:1.10.1")
+    implementation(platform("androidx.compose:compose-bom:2025.08.00"))
 
     // UI elements
-    implementation("androidx.compose.ui:ui:1.7.1")
-    implementation("androidx.compose.ui:ui-graphics:1.7.1")
-    implementation("androidx.compose.material3:material3:1.3.0")
+    implementation("androidx.compose.ui:ui:1.9.0")
+    implementation("androidx.compose.ui:ui-graphics:1.9.0")
+    implementation("androidx.compose.material3:material3:1.3.2")
 
     // Custom tabs support
-    implementation ("androidx.browser:browser:1.8.0")
+    implementation ("androidx.browser:browser:1.9.0")
 
     // ID token validation
     implementation ("org.bitbucket.b_c:jose4j:0.9.6")
 
     // API requests and JSON handling
-    implementation ("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation ("com.google.code.gson:gson:2.11.0")
+    implementation ("com.squareup.okhttp3:okhttp:5.1.0")
+    implementation ("com.google.code.gson:gson:2.13.1")
 }
